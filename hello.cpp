3f76c093b340149471c506ed7e53b026dc974615
santumetri <santu.metri@gmail.com>
2018-07-01 10:10:16 +0530

    file added

---
+++ hello.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+class print 
+{
+	public:
+		display()
+		{
+			cout<<"welcome to cpp world\n"<<endl;
+		}
+};
+
+
+int main ()
+{
+	print d1.display();
+	return 0;
+}
+
